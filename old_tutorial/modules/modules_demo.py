c98adf70a44b7dd686d50a168005e653bceb00bc
benq <beliy255@gmail.com>
2020-10-28 16:04:53 +0100

    reorganize the project

---
+++ old_tutorial/modules/modules_demo.py
@@ -0,0 +1,19 @@
+# modules
+
+from old_tutorial.methods import get_net_income as gni
+import math
+from math import sqrt
+
+
+class ModulesDemo:
+
+    def builtin_methods(self):
+        print(math.sqrt(100))
+        print(sqrt(100))
+
+
+m = ModulesDemo()
+m.builtin_methods()
+print()
+
+gni('Alabama', 1000)
